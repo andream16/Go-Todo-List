7c4f2865afe478ed34dde8d36bc1b758ce64768f
AndreaM16 <andrea.medda.uni@gmail.com>
2017-05-02 23:04:54 +0200

    Implemented Hashes and Lists insertions of sensed stuff.

--- api/addtodo.go
+++ api/addtodo.go
@@ -4,6 +4,8 @@ import (
 	"net/http"
 	"encoding/json"
 	"github.com/go-redis/redis"
+	"fmt"
+	"strconv"
 )
 
 func AddTodoHandler(c *redis.Client) func(w http.ResponseWriter, r *http.Request) {
@@ -21,10 +23,49 @@ func AddTodoHandler(c *redis.Client) func(w http.ResponseWriter, r *http.Request
 
 		defer r.Body.Close()
 
-		err = c.LPush("todos", todo.Content).Err()
-		if (err != nil) {
+		var uid = "0"
+		err = c.Get("i").Err()
+		if(err != nil) {
+		   	err := c.Set("i", "0", 0).Err()
+			if(err != nil){
+				panic(err)
+			}
+		} else {
+		   	index, err := c.Incr("i").Result()
+			uid = strconv.FormatInt(index, 10)
+		   	if(err != nil){
+			   	panic(err)
+		   	}
+
+			err = c.Set("i", index, 0).Err()
+			if( err!= nil ){
+				panic(err)
+			}
+		}
+
+		////////JUST A TEST BUT WORKS
+		var m = make(map[string]interface{})
+		m["id"] = uid
+		m["content"] = todo.Content
+
+		fmt.Println(uid)
+
+		hash, err := c.HMSet("k", m).Result()
+		if(err != nil){
 			panic(err)
 		}
+
+		mmap, err := json.Marshal(m)
+		if(err != nil){
+			panic(err)
+		}
+		s, _ := strconv.Unquote(string(mmap))
+		
+		err = c.LPush("mah", s).Err()
+		if(err != nil){
+			panic(err)
+		}
+		fmt.Println(hash)
 
 		w.WriteHeader(http.StatusOK)
 		w.Write(response)
